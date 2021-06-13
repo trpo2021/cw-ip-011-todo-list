ce9a2242507da53699346f35d86c962b2eddcad7
san4ous123 <sasha99928@mail.ru>
2021-06-13 19:58:40 +0300

    Fix codestyle

--- src/LibTODO/show_menu.cpp
+++ src/LibTODO/show_menu.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <LibTODO/show_menu.h>
-//show_menu - выводит меню в консоль
-void show_menu() {
-	std::cout << "[1] Add note." << std::endl;
-	std::cout << "[2] Show all notes." << std::endl;
-	std::cout << "[3] Find note." << std::endl;
-	std::cout << "[4] Remove one note." << std::endl;
-	std::cout << "[5] Remove all notes." << std::endl;
-	std::cout << "[6] Exit." << std::endl;
+#include <fstream>
+#include <iostream>
+#include <string>
+// show_menu - выводит меню в консоль
+void show_menu()
+{
+    std::cout << "[1] Add note." << std::endl;
+    std::cout << "[2] Show all notes." << std::endl;
+    std::cout << "[3] Find note." << std::endl;
+    std::cout << "[4] Remove one note." << std::endl;
+    std::cout << "[5] Remove all notes." << std::endl;
+    std::cout << "[6] Exit." << std::endl;
 }
