e4c4c7741f833c425e3e1dd79111c8895779b73f
san4ous123 <sasha99928@mail.ru>
2021-06-13 19:55:49 +0300

    Fix codestyle

--- src/LibTODO/all_notes.cpp
+++ src/LibTODO/all_notes.cpp
@@ -1,43 +1,44 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 //возвращает указатель на массив всех заметок
-std::string* all_notes(int& n_count) {
-	std::string* all_notes = nullptr;
-	std::string* all_notes_ptr = nullptr;
+std::string* all_notes(int& n_count)
+{
+    std::string* all_notes = nullptr;
+    std::string* all_notes_ptr = nullptr;
 
-	std::ifstream file("notes.txt");
-	if (!file.is_open()) {
-		std::cout << "File is not opened!";
-		exit(EXIT_FAILURE);
-	}
+    std::ifstream file("notes.txt");
+    if (!file.is_open()) {
+        std::cout << "File is not opened!";
+        exit(EXIT_FAILURE);
+    }
 
-	std::string one_note;
-	int count = 0;
-	while (!file.eof()) {
-		getline(file, one_note);
-		if (one_note.empty())
-			continue;
-		count++;
-		all_notes_ptr = new std::string[count];
+    std::string one_note;
+    int count = 0;
+    while (!file.eof()) {
+        getline(file, one_note);
+        if (one_note.empty())
+            continue;
+        count++;
+        all_notes_ptr = new std::string[count];
 
-		// опируем уже считанные заметки 
-		for (int i = 0; i < count - 1; i++)
-			all_notes_ptr[i] = all_notes[i];
+        // опируем уже считанные заметки
+        for (int i = 0; i < count - 1; i++)
+            all_notes_ptr[i] = all_notes[i];
 
-		//ƒобавл€ем новую заметку 
-		all_notes_ptr[count - 1] = one_note;
+        //ƒобавл€ем новую заметку
+        all_notes_ptr[count - 1] = one_note;
 
-		//ќчищаем старый массив all_notes
-		delete[] all_notes;
+        //ќчищаем старый массив all_notes
+        delete[] all_notes;
 
-		//all_notes указывает на новый массив 
-		all_notes = all_notes_ptr;
-		all_notes_ptr = nullptr;
-	}
+        // all_notes указывает на новый массив
+        all_notes = all_notes_ptr;
+        all_notes_ptr = nullptr;
+    }
 
-	file.close();
-	n_count = count;
+    file.close();
+    n_count = count;
 
-	return all_notes;
+    return all_notes;
 }
