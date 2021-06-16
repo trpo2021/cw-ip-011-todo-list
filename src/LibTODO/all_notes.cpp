4603d7fc2f5ec2c2f69d4f160bf528fd07c14e6c
berdnikov127 <berdnikov127@mail.ru>
2021-06-16 15:09:49 +0300

    edited code style

--- src/LibTODO/all_notes.cpp
+++ src/LibTODO/all_notes.cpp
@@ -6,13 +6,12 @@ std::string* all_notes(int& n_count)
 {
     std::string* all_notes = nullptr;
     std::string* all_notes_ptr = nullptr;
-
     std::ifstream file("notes.txt");
     if (!file.is_open()) {
         std::cout << "File is not opened!";
         exit(EXIT_FAILURE);
     }
-
+    
     std::string one_note;
     int count = 0;
     while (!file.eof()) {
@@ -21,24 +20,15 @@ std::string* all_notes(int& n_count)
             continue;
         count++;
         all_notes_ptr = new std::string[count];
-
-        //Копируем уже считанные заметки
         for (int i = 0; i < count - 1; i++)
             all_notes_ptr[i] = all_notes[i];
-
-        //Добавляем новую заметку
         all_notes_ptr[count - 1] = one_note;
-
-        //Очищаем старый массив all_notes
         delete[] all_notes;
-
-        // all_notes указывает на новый массив
         all_notes = all_notes_ptr;
         all_notes_ptr = nullptr;
     }
-
+    
     file.close();
     n_count = count;
-
     return all_notes;
 }
